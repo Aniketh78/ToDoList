9c600449de97a6875b63865b77e51e30e89e588b
Aniketh78 <231501014@rajalakshmi.edu.in>
2025-01-06 19:41:05 +0530

    Added 'Enter' to add Task, Progress Bar to keep track of completion

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tasks=JSON.parse(localStorage.getItem('tasks'))||[];
 updateToDo();
+countChecks();
 function updateToDo() {
     let todoHTML = '';
     for (let i = 0; i < tasks.length; i++) {
@@ -9,7 +10,8 @@ function updateToDo() {
             <div class="task">${tasks[i].task}</div>
             <button class="del-btn" onclick="
             tasks.splice(${i},1);
-            updateToDo();">Delete</button>
+            updateToDo();
+            countChecks();">Delete</button>
         </div>`;
     }
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -21,18 +23,40 @@ function updateToDo() {
             const taskId = checkbox.dataset.taskId;
             tasks[taskId].checked = checkbox.checked; 
             console.log(checkbox.checked);
-            localStorage.setItem('tasks', JSON.stringify(tasks)); 
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            countChecks(); 
         });
     });
 }
-
-
-const add_btn = document.querySelector('.add-btn');
-add_btn.addEventListener('click',()=>{
+const addTask = ()=>{
     const task = document.querySelector('.to');
     const tsk = task.value;
     tasks.push({task:tsk,checked:''});
     task.value ='';
     updateToDo();
+    countChecks();
 
-})
+};
+
+function chkEnter(event){
+    if(event.key==='Enter'){
+        addTask();
+    }
+}
+function countChecks(){
+    let count=0;
+    tasks.forEach((task)=>{
+        if(task.checked){
+            count+=1;
+        }
+    })
+    console.log(count);
+    const progressBar = document.querySelector('.elem-pres');
+    progressBar.style.width = (count/tasks.length)*100+'%';
+
+}
+
+
+const add_btn = document.querySelector('.add-btn');
+
+add_btn.addEventListener('click',addTask);
